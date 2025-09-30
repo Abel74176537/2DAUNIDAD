1066cc34dfd1b669f3b982690e617e0937951155
abel592 <abeljosuecolquehuanca12@gmail.com>
2025-09-30 13:52:28 -0500

    wyvcuw

--- ms-catalogo/src/main/java/com/example/mscatalogo/Entity/Categoria.java
+++ ms-catalogo/src/main/java/com/example/mscatalogo/Entity/Categoria.java
@@ -1,13 +1,14 @@
 package com.example.mscatalogo.Entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Categoria {
 
     @Id
