d39362077c552a23c858b7cd2d41fc23c9640516
abel592 <abeljosuecolquehuanca12@gmail.com>
2025-09-30 11:24:02 -0500

    ya debe dar

--- ms-producto/src/main/java/pe/edu/upeu/msproducto/feign/CatalogoFeign.java
+++ ms-producto/src/main/java/pe/edu/upeu/msproducto/feign/CatalogoFeign.java
@@ -2,6 +2,7 @@ package pe.edu.upeu.msproducto.feign;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,12 +24,13 @@ public interface CatalogoFeign {
     }
     @DeleteMapping("/{id}")
     @CircuitBreaker(name = "categoriaEliminarPorIdCB", fallbackMethod = "fallbackEliminarCategoria")
-    public void eliminar(@PathVariable Integer id);
+    ResponseEntity<String> eliminar(@PathVariable("id") Integer id);
 
-    default void fallbackEliminarCategoria(Integer id, Exception e) {
-        System.out.println("No se pudo eliminar la categoría con id " + id + ". Servicio temporalmente no disponible.");
-        // aquí podrías registrar en logs o manejar la lógica alternativa
+    default ResponseEntity<String> fallbackEliminarCategoria(Integer id, Exception e) {
+        String msg = "No se pudo eliminar la categoría con id " + id + ". Servicio temporalmente no disponible.";
+        return ResponseEntity.status(503).body(msg);
     }
+
 
 
 
